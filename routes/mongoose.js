fd8fbbc4be853dbabcfab70e14b58540b4705634
Ruttimanisk <badehat@gmail.com>
2025-03-25 12:22:03 +0100

    Create mongoose.js

---
+++ routes/mongoose.js
@@ -0,0 +1,26 @@
+// Import the mongoose module
+const mongoose = require("mongoose");
+
+// Set `strictQuery: false` to globally opt into filtering by properties that aren't in the schema
+// Included because it removes preparatory warnings for Mongoose 7.
+// See: https://mongoosejs.com/docs/migrating_to_6.html#strictquery-is-removed-and-replaced-by-strict
+mongoose.set("strictQuery", false);
+
+// Define the database URL to connect to.
+const mongoDB = "mongodb://127.0.0.1/my_database";
+
+// Wait for database to connect, logging an error if there is a problem
+main().catch((err) => console.log(err));
+async function main() {
+    await mongoose.connect(mongoDB);
+}
+
+// Define a schema
+const Schema = mongoose.Schema;
+
+const BooksSchema = new Schema({
+    a_string: String,
+    a_date: Date,
+});
+
+const Books = mongoose.model("Books", BooksSchema);
