335dfe4ef2392c83747dc8b3d3edf4661b026992
Madyca24 <mads@cajar.dk>
2025-04-06 15:06:06 +0200

    user model

--- models/user.js
+++ models/user.js
@@ -4,31 +4,30 @@ const {DateTime} = require("luxon");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    first_name: { type: String, required: true, minLength: 2, maxLength: 100 },
-    family_name: { type: String, required: true, minLength: 2, maxLength: 100 },
+    first_name: { type: String, required: true, maxLength: 50 },
+    family_name: { type: String, required: true, maxLength: 50 },
     date_of_birth: { type: Date, required: true },
     date_of_death: { type: Date },
     hourly_rate: { type: Number, required: true },
-    status: {
-        type: String,
-        required: true,
-        enum: ["employee", "admin"],
-    },
-    contract: { type: Schema.Types.ObjectId, ref: "Contract", required: true },
+    status: { type: String, required: true, enum: ["employee", "admin"] },
+    contract: { type: Schema.Types.ObjectId, ref: "Contract", required: true }, // maybe just store the contract here
+    username: { type: String, required: true, maxLength: 50 }, // not sure if we need a separate model for login details
+    password: { type: String, required: true, maxLength: 50 },
 });
 
+
 UserSchema.virtual("url").get(function () {
-    // We don't use an arrow function as we'll need the "this" object
     return `/${this.status}/profile/${this._id}`;
 });
+
 UserSchema.virtual("name").get(function () {
     let fullname = "";
     if (this.first_name && this.family_name) {
         fullname = `${this.family_name}, ${this.first_name}`;
     }
-
     return fullname;
 });
+
 UserSchema.virtual("lifespan").get(function() {
     return "" +(this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '') +
         (this.date_of_death ? " - " + DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '');
