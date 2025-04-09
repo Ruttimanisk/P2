8f8e338b0ddde5a0330db2e5c7b0d4c5a322d7f5
Madyca24 <mads@cajar.dk>
2025-04-09 15:02:47 +0200

    datetime GONE, nu er det strings!

--- models/userschedule.js
+++ models/userschedule.js
@@ -2,18 +2,18 @@ const mongoose = require("mongoose");
 const {DateTime} = require("luxon");
 
 const Schema = mongoose.Schema;
-const time_24 = DateTime.TIME_24_SIMPLE
 
+// DateTime.TIME_24_SIMPLE
 const UserScheduleSchema = new Schema ({
    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
    overtime: {type: Number, required: true},
    sick: {type: [Boolean, Number], default: [false, 0]}, // boolean and full days sick
    // start time, end time, optional comment - time uses format: xx:xx
-   monday: {type: [time_24, time_24, String], required: true},
-   tuesday: {type: [time_24, time_24, String], required: true},
-   wednesday: {type: [time_24, time_24, String], required: true},
-   thursday: {type: [time_24, time_24, String], required: true},
-   friday: {type: [time_24, time_24, String], required: true},
+   monday: {type: [String, String, String], required: true},
+   tuesday: {type: [String, String, String], required: true},
+   wednesday: {type: [String, String, String], required: true},
+   thursday: {type: [String, String, String], required: true},
+   friday: {type: [String, String, String], required: true},
 });
 
 UserScheduleSchema.virtual("url").get(function () {
@@ -23,12 +23,17 @@ UserScheduleSchema.virtual("url").get(function () {
 UserScheduleSchema.virtual("hours_this_week").get(function () {
     let sum = 0
     let week = [this.monday, this.tuesday, this.wednesday, this.thursday, this.friday]
-
     for(let i = 0; i < 5; i++){
-        let duration = week[i][0].diff(week[i][1])
-        sum += duration.as('hours')
+        let timeParts1 = week[i][0].split(":");
+        let time1 = Number(timeParts1[0]) * 60 + Number(timeParts1[1])
+        let timeParts2 = week[i][1].split(":");
+        let time2 = Number(timeParts2[0]) * 60 + Number(timeParts2[1])
+        if(time1 > time2){
+            return res.status(400).send('start time after end time')
+        } else {
+            sum += time2 - time1
+        }
     }
-
     return sum;
 });
 
