fcc15f4f99505e8729cdf54e4ed9b98ceb9a0190
Madyca24 <mads@cajar.dk>
2025-04-09 14:27:28 +0200

    fix datetime

--- models/userschedule.js
+++ models/userschedule.js
@@ -2,17 +2,18 @@ const mongoose = require("mongoose");
 const {DateTime} = require("luxon");
 
 const Schema = mongoose.Schema;
+const time_24 = DateTime.TIME_24_SIMPLE
 
 const UserScheduleSchema = new Schema ({
    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
    overtime: {type: Number, required: true},
    sick: {type: [Boolean, Number], default: [false, 0]}, // boolean and full days sick
    // start time, end time, optional comment - time uses format: xx:xx
-   monday: {type: [DateTime.TIME_24_SIMPLE, DateTime.TIME_24_SIMPLE, String], required: true},
-   tuesday: {type: [DateTime.TIME_24_SIMPLE, DateTime.TIME_24_SIMPLE, String], required: true},
-   wednesday: {type: [DateTime.TIME_24_SIMPLE, DateTime.TIME_24_SIMPLE, String], required: true},
-   thursday: {type: [DateTime.TIME_24_SIMPLE, DateTime.TIME_24_SIMPLE, String], required: true},
-   friday: {type: [DateTime.TIME_24_SIMPLE, DateTime.TIME_24_SIMPLE, String], required: true},
+   monday: {type: [time_24, time_24, String], required: true},
+   tuesday: {type: [time_24, time_24, String], required: true},
+   wednesday: {type: [time_24, time_24, String], required: true},
+   thursday: {type: [time_24, time_24, String], required: true},
+   friday: {type: [time_24, time_24, String], required: true},
 });
 
 UserScheduleSchema.virtual("url").get(function () {
