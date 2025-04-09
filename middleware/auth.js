584688758f8837ad211ce79d8aa5288258033397
Madyca24 <mads@cajar.dk>
2025-04-09 12:26:21 +0200

    auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const User = require('../models/user');
+
+exports.requireAuth = async (req, res, next) => {
+    const userId = req.cookies.userId;
+
+    if (!userId) {
+        return res.status(401).send('Not logged in');
+    }
+
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(401).send('User not found');
+        }
+
+        req.user = user;
+        next();
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal error');
+    }
+};
