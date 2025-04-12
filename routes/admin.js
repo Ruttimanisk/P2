9ae2ba104af6b84d974782a9eb45a022b0871490
Madyca24 <mads@cajar.dk>
2025-04-12 21:22:25 +0200

    Validation for admin user creation

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const user_controller = require("../controllers/user_controller");
 const userschedule_controller = require("../controllers/userschedule_controller");
+const {body} = require("express-validator");
 
 // med rotes herfra skal man gå ud fra at de allerede er på /admin/
 
@@ -18,8 +19,52 @@ router.get('/edit_schedule', userschedule_controller.admin_edit_schedule)
 
 router.get('/employee_list', user_controller.admin_employee_list)
 
-router.get('/user_creation', user_controller.admin_user_creation)
+router.get('/user_creation', (req, res) => { res.render('login') })
 
+router.post(
+    '/user_creation',
+    [
+        body("first_name", "First name must not be empty.")
+            .trim()
+            .isLength({ min: 1 })
+            .escape(),
+        body("family_name", "Family name must not be empty.")
+            .trim()
+            .isLength({ min: 1 })
+            .escape(),
+        body("date_of_birth", "Date of birth must not be empty.")
+            .trim()
+            .isLength({ min: 1 })
+            .escape(),
+        body("date_of_death").escape(),
+        body("address").escape(),
+        body("hours_per_week", "hours per week must not be empty.")
+            .trim()
+            .isNumeric()
+            .escape(),
+        body("hourly_rate", "hourly rate must not be empty.")
+            .trim()
+            .isNumeric()
+            .escape(),
+        body("role", "Role must not be empty.")
+            .trim()
+            .isLength({ min: 1 })
+            .escape(),
+        body("status", "Status must not be empty.")
+            .trim()
+            .isLength({ min: 1 })
+            .escape(),
+        body("contract").escape(),
+        body('username')
+            .trim()
+            .notEmpty().withMessage('Username is required')
+            .isLength({ max: 50 }).withMessage('Username too long'),
+        body('password')
+            .notEmpty().withMessage('Password is required')
+            .isLength({ max: 50 }).withMessage('Password too long'),
+    ],
+    user_controller.admin_user_creation
+);
 
 
 
