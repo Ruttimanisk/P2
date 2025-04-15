0cfdabf01e02d37162b3c244a0f45e573d4d98bb
Madyca24 <mads@cajar.dk>
2025-04-15 11:54:47 +0200

    fix view directory

--- controllers/userschedule_controller.js
+++ controllers/userschedule_controller.js
@@ -4,8 +4,13 @@ const asyncHandler = require("express-async-handler");
 
 exports.shared_schedule = asyncHandler( async (req, res, next) => {
     const allSchedules = await UserSchedule.find().exec();
-    res.render(`${req.user.status}_schedule`, {
-        title: "Schedule",
-        shared_schedule: allSchedules,
-    });
+    res.redirect(`/schedule`)
 });
+
+exports.csv_input_data = asyncHandler( async (req, res) => {
+    const allSchedules = await UserSchedule.find().exec();
+    let input = []
+    input.forEach((schedule) => {
+        
+    })
+})
