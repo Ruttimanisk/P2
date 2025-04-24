10e84d3618f8ebb49f36ce63ee7af63c4ff308e1
TueElhegn <telheg24@student.aau.dk>
2025-04-24 17:49:12 +0200

    fullcalendar.io test

--- routes/admin.js
+++ routes/admin.js
@@ -13,20 +13,16 @@ const {body} = require("express-validator");
 app.get('/calendar', (req, res) => {
         const events = [
                 {
-                        title: 'Test Event',
+                        title: 'Test Vagt',
                         start: '2025-04-24T10:00:00',
                         end: '2025-04-24T11:00:00'
-                },
-                {
-                        title: 'Møde',
-                        start: '2025-04-25T13:00:00',
-                        end: '2025-04-25T14:30:00'
                 }
         ];
 
         console.log("📦 Events before render:", events);
         res.render('calendar', { events: JSON.stringify(events) });
 });
+
 
 
 
