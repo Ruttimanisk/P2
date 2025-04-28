8f005b284bee01187e6a7fa66d0f7e3e903e937b
Ruttimanisk <badehat@gmail.com>
2025-04-28 11:19:17 +0200

    peter leger med unittest

--- tests/test.py
+++ tests/test.py
@@ -4,7 +4,7 @@ import algorithm.schedule
 class TestAlgorithm(unittest.TestCase):
 
     def test_shift_hours(self):
-        shifthours = algorithm.schedule.shift_hours('8:00', '16:00')
-        self.assertEqual(shifthours, 8.0, 'Subtraction is wrong')
+        shifthours = algorithm.schedule.shift_hours('8:30', '16:00')
+        self.assertEqual(shifthours, 7.5, 'Subtraction is wrong')
 if __name__ == '_main__':
     unittest.main()
