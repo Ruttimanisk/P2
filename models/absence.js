9a3c00c80eb7d8bfa27098f00433a59c897a13b9
Madyca24 <mads@cajar.dk>
2025-04-30 11:56:57 +0200

    Absence

--- models/absence.js
+++ models/absence.js
@@ -11,11 +11,11 @@ const AbsenceSchema = new Schema({
 });
 
 AbsenceSchema.virtual("leave_start_short").get(function() {
-    return "" +(this.leave_start ? DateTime.fromJSDate(this.leave_start).toLocaleString({ weekday: 'long', month: 'short', day: '2-digit' }) : '')
+    return "start: " +(this.leave_start ? DateTime.fromJSDate(this.leave_start).toLocaleString({ weekday: 'long', month: 'short', day: '2-digit' }) : '')
 });
 
 AbsenceSchema.virtual("leave_end_short").get(function() {
-    return "" +(this.leave_end ? DateTime.fromJSDate(this.leave_end).toLocaleString({ weekday: 'long', month: 'short', day: '2-digit' }) : '')
+    return "/ end: " +(this.leave_end ? DateTime.fromJSDate(this.leave_end).toLocaleString({ weekday: 'long', month: 'short', day: '2-digit' }) : '')
 });
 
 module.exports = mongoose.model("Absence", AbsenceSchema);
