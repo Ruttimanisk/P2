7ec3e10e1f154730077dac8416642690f36f9244
Madyca24 <mads@cajar.dk>
2025-04-30 11:14:52 +0200

    Absence

--- models/user.js
+++ models/user.js
@@ -32,8 +32,8 @@ UserSchema.virtual("fullname").get(function () {
 });
 
 UserSchema.virtual("lifespan").get(function() {
-    return "" +(this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString({ weekday: 'long', month: 'short', day: '2-digit' }) : '') +
-        (this.date_of_death ? " - " + DateTime.fromJSDate(this.date_of_death).toLocaleString({ weekday: 'long', month: 'short', day: '2-digit' }) : '');
+    return "" +(this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '') +
+        (this.date_of_death ? " - " + DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '');
 });
 
 
