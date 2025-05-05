1696f3a46c528c67aa08fff5aba40df66bc53df2
Madyca24 <mads@cajar.dk>
2025-05-05 15:02:46 +0200

    update profile

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -387,6 +387,7 @@ exports.update_profile_post = asyncHandler(async (req,res) => {
 
     try {
         const user = new User({
+            _id: user_old._id,
             first_name: req.body.first_name,
             family_name: req.body.family_name,
             date_of_birth: req.body.date_of_birth,
