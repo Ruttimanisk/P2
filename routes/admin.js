cee43f115b4a07c0375fc63c96bc69ac29c2acc2
Madyca24 <mads@cajar.dk>
2025-05-23 10:34:47 +0200

    reload when generating schedule

--- routes/admin.js
+++ routes/admin.js
@@ -19,8 +19,7 @@ const User = require('../models/user');
 router.post('/run_algorithm', async (req, res) => {
     const arg = req.body?.param;
     runpy(String(arg));
-    res.json({ message: 'Algorithm started with param: ' + arg });
-    res.redirect('back');
+    res.json({ redirect: 'back' });
 });
 
 // Mads lav en controller til det her!!
